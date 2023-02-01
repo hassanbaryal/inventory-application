14ad39a95cb3b866b33e445f4555bb571f5ecc50
hassanbaryal <h.baryal2@gmail.com>
2023-02-01 14:41:13 -0500

    Connected to MongoDB database

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+const mongoDB =
+  'mongodb+srv://associate-developer:Adev-1234@cluster0.mfpqnkb.mongodb.net/inventory_app?retryWrites=true&w=majority';
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch((err) => {
+  throw new Error(err);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
