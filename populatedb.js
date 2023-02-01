ad302875fef68c7fc9de6386e5630fd332534dc3
hassanbaryal <h.baryal2@gmail.com>
2023-02-01 18:06:11 -0500

    Updated console.log at tope of file. Updated connection to mongodb database

--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 console.log(
-  'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true'
+  'This script populates some test cars, brands, car types and carinstances to the database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true'
 );
 
 // Get arguments passed on command line
@@ -20,11 +20,16 @@ const Brand = require('./models/brand');
 const CarType = require('./models/carType');
 const CarInstance = require('./models/carInstance');
 
+mongoose.set('strictQuery', false); // Prepare for Mongoose 7
+
 const mongoDB = userArgs[0];
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch(err => console.log(err));
 
 const brands = [];
 const carTypes = [];
