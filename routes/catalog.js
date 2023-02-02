c58a5548891bde5507483979ab489dd7c86381c7
hassanbaryal <h.baryal2@gmail.com>
2023-02-02 15:28:52 -0500

    Added handler function to /cars route

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,4 +6,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', carController.index);
 
+// GET all cars page
+router.get('/cars', carController.cars_list);
+
 module.exports = router;
