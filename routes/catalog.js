fdc2c08ad29a6ec184e1c454be553e302e00bb2a
hassanbaryal <h.baryal2@gmail.com>
2023-02-02 11:07:55 -0500

    Created catalog router to handle all routes for application

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const carController = require('../controllers/carController');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.redirect('/catalog');
-});
+router.get('/', carController.index);
 
 module.exports = router;
